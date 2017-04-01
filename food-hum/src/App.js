50c28a15e096d4557d3e9258fa34b63a8c95eb62
Adebiyi Oluwatobi <tobiemmanuel44@yahoo.com>
2017-04-01 02:13:18 -0500

    Revert "Setting up ignore"
    This reverts commit c126de1d217fa183e958223514a2f0b74c82c60a.
    
    # Conflicts:
    #	.idea/workspace.xml

--- food-hum/src/App.js
+++ food-hum/src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
 import logo from './images/FoodHUM.png';
-import { Grid, Row, Col, FormControl, FormGroup, ControlLabel, HelpBlock } from 'react-bootstrap';
 
 class App extends Component {
-
-    getValidationState() {
-        const length = this.state.value.length;
-        if (length > 10) return 'success';
-        else if (length > 5) return 'warning';
-        else if (length > 0) return 'error';
-    }
-
-    handleChange(e) {
-        this.setState({ value: e.target.value });
-    }
-
   render() {
-      return (
-          <div>
-              
-          </div>
-      );
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Live into the future with better food choices</p>
+        </div>
+        <div>
+            <form>
+                <div class="row">
+                    <div class="medium-6">
+                            <input type="text" placeholder="Name of Restaurant"/>
+                    </div>
+                    <div class="medium-6">
+                        <input type="text" placeholder="Location"/>
+                    </div>
+                </div>
+            </form>
+        </div>
+      </div>
+    );
   }
 }
 
